a311bf528ec470917663c13c6f688b7351cf1589
Anemone2198 <anemone2198@gmail.com>
2024-09-15 17:28:49 +0000

    feat : add free class doctype

---
+++ athena_dev/athena_dev/doctype/free_class/free_class.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, anemone2198@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Free Class", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ athena_dev/athena_dev/doctype/free_class/free_class.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, anemone2198@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FreeClass(Document):
+	pass
